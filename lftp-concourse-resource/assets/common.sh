a6dec2ff432aaceea967efd61b9486ce9f0c2f17
Curtis Beeson <clbeeson@Phenom6Debian.domain>
2018-02-11 11:37:20 -0600

    added images

---
+++ lftp-concourse-resource/assets/common.sh
@@ -0,0 +1,20 @@
+parse_source_config() {
+  URL=$(jq -r '.source.url // "https://download.opensuse.org"' $payload)
+  REGEXP=$(jq -r '.source.regexp // ""' $payload)
+  VERSION=$(jq -r '.version.ref // ""' $payload)
+}
+
+parse_param_config() {
+  TAR=$(jq -r '.params.tar // "false"' $payload)
+  TARPREFIX=$(jq -r '.params.tar // "file"' $payload)
+}
+
+get_listing() {
+  GLOB=$(echo "$REGEXP" | sed 's/(.*)/*/')
+  LISTING=$(lftp -c "open $URL; set cmd:cls-default ''; cls -1 $GLOB")
+}
+
+parse_versions() {
+  REG=$(echo "$REGEXP" | sed -e 's/\./\\./g' -e 's/(.*)/\\([0-9.-]*\\)/')
+  FOUND_VERSIONS=$(echo $LISTING | sed -ne "s/$REG/\1/p")
+}
